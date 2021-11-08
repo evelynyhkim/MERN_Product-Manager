8c65b6722061db8b7a6a711e164ccc08af848e6e
Evelyn Kim <evelyn.thkim@gmail.com>
2021-11-07 21:50:20 -0800

    table applied to prod list

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,24 +1,32 @@
 import React from "react"
 //import styles from "./NewProduct.module.css"
 //import axios from 'axios'
-import {navigate, Router, Link} from '@reach/router'
+import {navigate, Link} from '@reach/router'
 import DeleteButton from "./DeleteButton"
 
 function ProductList({prods, callback}) {
     function handleEdit(id){
         navigate(`/${id}/edit`)
     }
+    const style = {
+        border: "solid black 1px", 
+        borderCollapse: "collapse",
+        margin: "0 auto"
+    }
 
     return (
-        prods.map((prod, idx)=>(
-            <div key={idx}>
-                <Link to={`${prod._id}`} id={prod._id}>{prod.title}</Link>
-                    
-                    <button onClick={()=>handleEdit(prod._id)} 
-                    style={{marginLeft: "10px", height: "20px", padding: "3px"}}>Edit</button>
-                    <DeleteButton id={prod._id} callback={callback}/>
-                    </div>
-    )))
+        <table style={style}>
+        {prods.map((prod, idx)=>(
+             <tr key={idx} style={style}>
+                <td style={style}><Link to={`${prod._id}`} id={prod._id}>{prod.title}</Link>
+                </td>
+                <td style={style}><button onClick={()=>handleEdit(prod._id)}>
+                    Edit</button>
+                <DeleteButton id={prod._id} callback={callback}/>
+                </td>
+            </tr>               
+        ))}
+        </table>)
 }
 export default ProductList
                 // <button onClick={()=>handleDelete(prod._id)} 
